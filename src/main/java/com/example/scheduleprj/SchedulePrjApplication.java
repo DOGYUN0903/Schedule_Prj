0d71b8e74a06cd245853e6c19c22912656c0a299
SexyDoGyun <kimdk0903@naver.com>
2025-05-12 14:37:22 +0900

    chore: 프로젝트 진입점 클래스 생성

---
+++ src/main/java/com/example/scheduleprj/SchedulePrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulePrjApplication.class, args);
+    }
+
+}
