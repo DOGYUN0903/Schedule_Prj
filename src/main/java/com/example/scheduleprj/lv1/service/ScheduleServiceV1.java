42e014561c426d54083f92b0d0d3b974d7a09071
SexyDoGyun <kimdk0903@naver.com>
2025-05-12 15:21:44 +0900

    feat: 전체 일정 조회 서비스 인터페이스 정의

--- src/main/java/com/example/scheduleprj/lv1/service/ScheduleServiceV1.java
+++ src/main/java/com/example/scheduleprj/lv1/service/ScheduleServiceV1.java
@@ -3,7 +3,11 @@ package com.example.scheduleprj.lv1.service;
 import com.example.scheduleprj.lv1.dto.ScheduleRequestDto;
 import com.example.scheduleprj.lv1.dto.ScheduleResponseDto;
 
+import java.util.List;
+
 public interface ScheduleServiceV1 {
 
     ScheduleResponseDto saveSchedule(ScheduleRequestDto requestDto);
+
+    List<ScheduleResponseDto> findAllSchedules();
 }
