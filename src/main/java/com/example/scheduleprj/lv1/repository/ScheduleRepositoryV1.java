115df88af87bb9cc31ee93e73c8edfac18a33c09
SexyDoGyun <kimdk0903@naver.com>
2025-05-12 15:22:18 +0900

    feat: 전체 일정 조회 레포지토리 인터페이스 정의

--- src/main/java/com/example/scheduleprj/lv1/repository/ScheduleRepositoryV1.java
+++ src/main/java/com/example/scheduleprj/lv1/repository/ScheduleRepositoryV1.java
@@ -3,6 +3,10 @@ package com.example.scheduleprj.lv1.repository;
 import com.example.scheduleprj.lv1.dto.ScheduleResponseDto;
 import com.example.scheduleprj.lv1.entity.Schedule;
 
+import java.util.List;
+
 public interface ScheduleRepositoryV1 {
     ScheduleResponseDto saveSchedule(Schedule schedule);
+
+    List<ScheduleResponseDto> findAllSchedules();
 }
